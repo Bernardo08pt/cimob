04c949adace54041a46bd6bfef1632a4cc2ad59a
Bernardo08pt <bernardoamguerreiro@gmail.com>
2017-11-28 20:33:52 +0000

    Changed landing page route Added landig page layout

--- cimob/Startup.cs
+++ cimob/Startup.cs
@@ -61,8 +61,8 @@ namespace cimob
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Account}/{action=Login}/{id?}");
             });
         }
-    }
+    } 
 }
