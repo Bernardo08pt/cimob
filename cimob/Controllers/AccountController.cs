db16e9ae1cc32b5adb882a8e59ae6c964b97edd1
Bernardo08pt <bernardoamguerreiro@gmail.com>
2017-12-12 12:05:38 +0000

    Fixes

--- cimob/Controllers/AccountController.cs
+++ cimob/Controllers/AccountController.cs
@@ -166,11 +166,11 @@ namespace cimob.Controllers
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
-
+                    
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     var callbackUrl = Url.EmailConfirmationLink(user.Id, code, Request.Scheme);
                     await _emailSender.SendEmailConfirmationAsync(model.Email, callbackUrl);
-
+                    
                     await _signInManager.SignInAsync(user, isPersistent: false);
                     _logger.LogInformation("User created a new account with password.");
                     return RedirectToLocal(returnUrl);
