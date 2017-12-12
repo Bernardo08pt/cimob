a969bb6743c7757d457f05451781b537f429cf51
andreia-novas <andreianovas93@gmail.com>
2017-12-12 22:48:47 +0000

    PopoverHide

--- cimob/wwwroot/js/site.js
+++ cimob/wwwroot/js/site.js
@@ -1,2 +1,3 @@
 ﻿// Write your JavaScript code.
-$('.pop').webuiPopover({ style: 'inverse', animation: 'pop', trigger: 'hover', placement: 'top'});
+$('.pop').webuiPopover({
+    style: 'inverse', animation: 'pop', trigger: 'hover', placement: 'top', delay: { show: null, hide: 500 }});
