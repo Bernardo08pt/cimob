4db239225072372aaaa677d36ec35b804d772b47
Bernardo08pt <bernardoamguerreiro@gmail.com>
2017-12-15 15:44:01 +0000

    javascript fix

--- cimob/wwwroot/js/site.js
+++ cimob/wwwroot/js/site.js
@@ -1,6 +1,5 @@
 ﻿// Write your JavaScript code.
-$('.pop').webuiPopover({
-    style: 'inverse', animation: 'pop', trigger: 'hover', placement: 'top', width: 300, delay: { show: null, hide: 500 }});
-});
+$('.pop').webuiPopover({style: 'inverse', animation: 'pop', trigger: 'hover', placement: 'top', width: 300, delay: { show: null, hide: 500 }});
+
 
 $(document).on('click', '#logout', function () { $('#logoutForm').submit(); });
