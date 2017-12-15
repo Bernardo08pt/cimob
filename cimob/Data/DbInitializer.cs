c8570ba0a8c6e6a26490a3fe1e08689bcd0cc41f
Bernardo08pt <bernardoamguerreiro@gmail.com>
2017-12-15 15:17:20 +0000

    Added more default help

--- cimob/Data/DbInitializer.cs
+++ cimob/Data/DbInitializer.cs
@@ -21,7 +21,8 @@ namespace cimob.Data
             var ajudas = new Ajuda[]
             {
                new Ajuda{Nome="Email", Titulo="Endereço Email", Corpo="<p>O Email é necessário para efetuar o login na aplicação.</p><p> Este deve ser validado através de um link enviado para o mesmo.</p>"},
-               new Ajuda{Nome="Password", Titulo="Password", Corpo="<p>A Password é necessária para efetuar o login na aplicação.</p><p> Esta deve conter um mínimo de 6 caracteres.</p>"}
+               new Ajuda{Nome="Password", Titulo="Password", Corpo="<p>A Password é necessária para efetuar o login na aplicação.</p><p> Esta deve conter um mínimo de 6 caracteres.</p>"},
+               new Ajuda{Nome="Numero", Titulo="Número", Corpo="<p>O Número corresponde ao número de aluno/docente do IPS.</p>"}
             };
             foreach (Ajuda a in ajudas)
             {
