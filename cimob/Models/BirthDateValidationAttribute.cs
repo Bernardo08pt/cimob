b558d97ccf8d24d359edb051a1b5eff633488806
Bernardo08pt <bernardoamguerreiro@gmail.com>
2017-12-20 21:12:42 +0000

    Created client side date validation

--- cimob/Models/BirthDateValidationAttribute.cs
+++ cimob/Models/BirthDateValidationAttribute.cs
@@ -7,8 +7,9 @@ using System.Threading.Tasks;
 
 namespace cimob.Models
 {
-    public class BirthDateValidationAttribute : ValidationAttribute
+    public class BirthDateValidationAttribute : ValidationAttribute, IClientModelValidator
     {
+
         public override bool IsValid(object value)
         {
             DateTime dataLimiteMaxima = (DateTime.Now.AddYears(-17));
@@ -18,6 +19,25 @@ namespace cimob.Models
 
             return valor.Date < dataLimiteMaxima.Date && valor.Date > dataLimiteMinima.Date;
         }
+        public void AddValidation(ClientModelValidationContext context)
+        {
+            MergeAttribute(context.Attributes, "data-val", "true");
+            var errorMessage = FormatErrorMessage(context.ModelMetadata.GetDisplayName());
+            MergeAttribute(context.Attributes, "data-val-dataoffparameters", errorMessage);
+        }
 
+        private bool MergeAttribute(
+            IDictionary<string, string> attributes,
+            string key,
+            string value)
+        {
+            if (attributes.ContainsKey(key))
+            {
+                return false;
+            }
+            attributes.Add(key, value);
+            return true;
+        }
     }
+    
 }
