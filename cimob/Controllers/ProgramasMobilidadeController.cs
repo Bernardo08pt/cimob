2c60ebb262fac01eb89ee72f7cdfd9ce7340be1b
Andreia Pereira <andreiiap02@hotmail.com>
2018-01-11 21:49:10 +0000

    adicionado o modal dos programas de mobilidade

--- cimob/Controllers/ProgramasMobilidadeController.cs
+++ cimob/Controllers/ProgramasMobilidadeController.cs
@@ -8,7 +8,14 @@ namespace cimob.Controllers
 {
     public class ProgramasMobilidadeController : Controller
     {
+
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        //Método que retorna o modal dos programas de mobilidade
+        public ActionResult ModalPopUp()
         {
             return View();
         }
---
+++ cimob/wwwroot/js/pop-up-modal.js
@@ -0,0 +1,33 @@
+﻿$(document).ready(function () {
+    $("#btnCancelar").click(function () {
+        $("#programModal").modal('hide');
+    });
+
+    function openModal(tituloConteudo) {
+        $("#programModal").modal('show');
+        var titulo = document.getElementById('titulo');
+        titulo.textContent = tituloConteudo;
+    }
+
+    /*Adicionei o click a cada button para mostrar o título respetivo*/
+    $("#erasmus-modal").click(function () {
+        openModal('Erasmus+')
+    });
+
+    $("#luso-modal").click(function () {
+        openModal('Bolsas Luso-Brasileiras Santander Universidades')
+    });
+
+    $("#ibero-modal").click(function () {
+        openModal('Bolsas Ibero-Americanas Santander Universidades')
+    });
+
+    $("#macau-modal").click(function () {
+        openModal('Cooperação com o Instituto Politécnico de Macau')
+    });
+
+    $("#vasco-gama-modal").click(function () {
+        openModal('Mobilidade Vasco da Gama')
+    });
+
+});
