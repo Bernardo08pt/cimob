00d281e6ec8241aa0724dd8c440a49eda272c50f
Andreia Pereira <andreiiap02@hotmail.com>
2018-01-12 02:14:42 +0000

    Modal dos programas de mobilidade

--- cimob/wwwroot/js/pop-up-modal.js
+++ cimob/wwwroot/js/pop-up-modal.js
@@ -3,13 +3,14 @@
         $("#programModal").modal('hide');
     });
 
+    /*USA-SE ISTO TUDO SE FOR PARA TODOS TEREM O MODAL
     function openModal(tituloConteudo) {
         $("#programModal").modal('show');
         var titulo = document.getElementById('titulo');
         titulo.textContent = tituloConteudo;
     }
 
-    /*Adicionei o click a cada button para mostrar o título respetivo*/
+    /*Adicionei o click a cada button para mostrar o título respetivo
     $("#erasmus-modal").click(function () {
         openModal('Erasmus+')
     });
@@ -28,6 +29,33 @@
 
     $("#vasco-gama-modal").click(function () {
         openModal('Mobilidade Vasco da Gama')
+    });*/
+
+
+
+    /*Enviar o tipo de mobilidade e o período de estudos ou estágio*/
+    $("#btnSubmit").on("click", (ev) => {
+        ev.preventDefault();
+        /*TODO: no estágio está a dar undefined*/
+        window.location = "/Application?tipo_mobilidade=tipo_mobilidade&estagio= " +
+            $("input[type='radio']").find("[name='periodo']").val() + "'";
     });
+
+    /*$("#erasmus-modal").click(function () {
+        $("#programModal").modal('show');
+    });*/
+    $(".main-div a").on("click", (ev) => {
+        var elem = ev.currentTarget;
+        // vês se é erasmus 
+        // se for abres o popup e fazes
+        if (elem.getAttribute("id") == "erasmus-modal") {
+          $("#programModal").modal('show');
+        } else {
+
+            // senão
+            window.location = "/Application?tipo_mobilidade=" + elem.getAttribute("data-id")
+        }
+
+    })
 
 });
