3b204d43f9578090f491204df5092966ad98cd4b
Personal <tiagoribeiro.1803@gmail.com>
2018-01-17 23:26:25 +0100

    Redirect based on user role

--- cimob/Controllers/HomeController.cs
+++ cimob/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using cimob.Models;
 using Microsoft.AspNetCore.Authorization;
@@ -14,21 +10,11 @@ namespace cimob.Controllers
     {
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
+            if (User.IsInRole("Funcionario"))
+                return RedirectToAction("Index", "ServicosCimob");
+            
+            else 
+                return RedirectToAction("Profile", "Manage");
         }
 
         public IActionResult Error()
