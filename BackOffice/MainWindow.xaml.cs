8a9bf8a85294ba1036dc2145772c40e100e04a13
andreia-novas <andreianovas93@gmail.com>
2018-02-08 16:14:27 +0000

    Page Visualizar Graficos

--- BackOffice/MainWindow.xaml.cs
+++ BackOffice/MainWindow.xaml.cs
@@ -24,5 +24,13 @@ namespace BackOffice
         {
             InitializeComponent();
         }
+
+        private void AbrirMenu_Click(object sender, RoutedEventArgs e)
+        {
+            (sender as Button).ContextMenu.IsEnabled = true;
+            (sender as Button).ContextMenu.PlacementTarget = (sender as Button);
+            (sender as Button).ContextMenu.Placement = System.Windows.Controls.Primitives.PlacementMode.Bottom;
+            (sender as Button).ContextMenu.IsOpen = true;
+        }
     }
 }
